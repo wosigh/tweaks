da8701b451d710dec7bfdbe7325647f6da27701d
Janne Julkunen <scorpio.iix@gmail.com>
2011-04-09 04:01:36 +0300

    Update the version properly

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -11,7 +11,7 @@ function StartupAssistant() {
 
 	this.newMessages = [
 	{
-		version: '0.0.9', log: [
+		version: '0.1.0', log: [
 			'<b>Requires WebOS 2.0.0 or newer to work!</b>',
 			'Initial release of Tweaks app for WebOS'
 		]
