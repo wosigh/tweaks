e65b0ddb859cd7f18681a3a17899f7a9fd84958b
Rod Whitby <rod@whitby.id.au>
2011-04-12 17:47:02 +0930

    Allow the tweaks preferences file to override the target owner

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -12,12 +12,13 @@ function StartupAssistant() {
 	this.secondMessage = $L("Please consider making a <a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZFYPT3NYR36YJ\">donation</a> if you wish to show your appreciation.");
 
 	this.newMessages = [
-	{
+	 {	version: '1.1.0', log: [ 'Allow for the tweaks json file to override the owner value' ] },
+	 {
 		version: '1.0.0', log: [
 			'<b>Requires webOS 2.0.0 or newer to work!</b>',
 			'Initial release of Tweaks app for webOS'
 		]
-	}];
+	 }];
 	
 	this.menuModel = {
 		visible: true,
